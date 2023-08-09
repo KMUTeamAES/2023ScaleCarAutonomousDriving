27cdae99b42dba0c9993d6bb6144d6f7b6d8df56
lee jung yong <jung58567@naver.com>
2023-08-10 02:28:30 +0900

    Hough

---
+++ src/hojun.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+class lane_detect:
+	def __init__(self):
+		rospy.init_node('hojun')
+		rospy.Subscriber("/usb_cam/image_rect_color",Image,self.image_CB)
+		self.bridge = CvBridge()
+		
+		self.left_top = (285,270)
+		self.left_bottom = (0,480)
+		self.right_bottom = (640,480)
+		self.right_top = (378,270)
+		self.src_points = np.float32([self.left_top,self.left_bottom,self.right_bottom,self.right_top])
+		self.dst_points = np.float32([(160, 0),(160, 480),(480, 480),(480, 0)])
+	def image_CB(self,data):
+		try:
+			frame = self.bridge.imgmsg_to_cv2(data,desired_encoding="bgr8")
+			height, width = frame.shape[:2]
+			lines = self.find_lanes(frame)
+			bird_view = self.bird_eye_view(frame, height, width)
+
+
+			# cv2.imshow("Hough_lane",bird_view)
+			cv2.imshow("OG_frame",frame)
+			cv2.waitKey(1)
+			cv2.destroyAllwindows()
+		except Exception as e:
+			print("Error:", e)
+	# def calculate_angle(self,lines):
+		# for line in lines:
+
+
+
+	def bird_eye_view(self,frame,height,width):
+		M = cv2.getPerspectiveTransform(self.src_points, self.dst_points)
+		warp_frame = cv2.warpPerspective(frame,M,(width,height))
+		return warp_frame
+
+	def find_lanes(self,image):
+	    # 그레이스케일 이미지로 변환
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	
+	    # 엣지 검출
+		edges = cv2.Canny(gray,threshold1=50, threshold2=150)
+
+	    # ROI(Region of Interest) 설정
+		height, width = edges.shape[:2]
+		roi_vertices = [(0, height), (width / 2, 0), (width, height)]
+		mask = np.zeros_like(edges)
+		cv2.fillPoly(mask, np.array([roi_vertices], np.int32), 255)
+		masked_edges = cv2.bitwise_and(edges, mask)
+	
+	    # 허프 변환을 통한 직선 검출
+		lines = cv2.HoughLinesP(masked_edges, rho=1, theta=np.pi/180, threshold=100, minLineLength=100, maxLineGap=100)
+		#threshold값 낮을수록 직선 더 잘 검출 
+		#lines라는 변수는 2차원 배열 (x1,y1,x2,y2)가 행에 저장돼 있음. 즉 무한 by 4 형태
+		line_image = np.zeros_like(image)
+		if lines is not None:
+			print("line found")
+			for line in lines:
+				x1, y1, x2, y2 = line[0]
+				cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=2)
+		else:
+			print("line not found")
+		#원본 이미지와 직선 이미지 합치기
+		# result = cv2.addWeighted(image,0.8,line_image,1,0)
+	    
+		# cv2.imshow("Hough_lane",result)
+		# cv2.waitKey(1)
+		# cv2.destroyAllwindows()
+		return lines
+
+def main():
+	start = lane_detect()
+	rospy.spin()
+
+if __name__=="__main__":
+	main()
