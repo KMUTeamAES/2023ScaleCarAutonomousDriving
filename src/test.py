fe7af8f06fa4ca7faa15181874287f47ee05727c
lee jung yong <jung58567@naver.com>
2023-08-09 18:35:13 +0900

    clean

--- src/test.py
+++ src/test.py
@@ -18,14 +18,8 @@ class LaneDetectorWebot:
         self.left_bottom = (0,480)
         self.right_bottom = (640,480)
         self.right_top = (378,270)
-
-        # self.left_top = (260,270)
-        # self.left_bottom = (0,480)
-        # self.right_bottom = (640,480)
-        # self.right_top = (393,270)
         
         self.src_points = np.float32([self.left_top,self.left_bottom,self.right_bottom,self.right_top])
-        # self.dst_points = np.float32([(320, 0), (320, 720), (960, 720), (960, 0)])
         self.dst_points = np.float32([(160, 0),(160, 480),(480, 480),(480, 0)])
         # Define other constants
         self.win_bottom_y = 0  # fixed value
@@ -33,7 +27,6 @@ class LaneDetectorWebot:
         self.max_Theta = 0.1
         self.min_Theta = -0.1
         self.aver_Theta = 0.0
-        self.default_angle = 0.5  # radian
         self.k1 = 0.01
         self.k2 = 0.01
         self.bridge = CvBridge()
@@ -46,44 +39,25 @@ class LaneDetectorWebot:
             height, width = frame.shape[:2]
             
             bird_eye_view = self.bird_eye_view_transform(frame, height, width)
-
-            # bird_line_view = self.detect_lane_lines(bird_eye_view)
             
             white_image = self.image_filtering(bird_eye_view)
 
-            
             left_x_base, right_x_base = self.histogram(white_image)
             
             out_image, left_bottom_x, left_top_x, right_bottom_x, right_top_x = self.sliding_windows(white_image,left_x_base, right_x_base)
             
             Theta = self.cal_ang(white_image,left_bottom_x,left_top_x,right_bottom_x,right_top_x)
             
-            steer_angle = self.decion_steer_ang(Theta)
+            steer_angle = self.decision_steer_ang(Theta)
             print("steer_angle = ", steer_angle)
             
-            
-            # ref_distance = self.find_ref_distance(left_x_base, right_x_base)
-            # ref_angle = self.find_ref_angle(left_bottom_x, left_top_x, right_bottom_x, right_top_x)
-            # steer_angle = (self.k1 * ref_distance) + (self.k2 * ref_angle)
-            # print("steer_angle = ", steer_angle)
             cv2.imshow("OG_frame", frame)
-
-            # cv2.imshow("bird_eye_view", bird_eye_view)
             cv2.imshow("Result", out_image)
 
-            # self.steer_msg.data = steer_angle
             self.steer_msg.data = steer_angle
             rospy.sleep(2)
             self.steer_angle_pub.publish(self.steer_msg.data)
             self.rate.sleep()
-            # cv2.circle(frame, self.left_top, 5, (0,0,255), -1)
-            # cv2.circle(frame, self.left_bottom, 5, (0,0,255), -1)
-            # cv2.circle(frame, self.right_bottom, 5, (0,0,255), -1)
-            # cv2.circle(frame, self.right_top, 5, (0,0,255), -1)
-            # cv2.imshow("line_detect",bird_line_view)
-                        
-            end = time.time()
-            print("end-start",end-start)
 
             cv2.waitKey(1)
             if 0xFF == ord('q'):
@@ -92,7 +66,7 @@ class LaneDetectorWebot:
         except Exception as e:
             print("Error:", e)
 
-    def decion_steer_ang(self,Theta):
+    def decision_steer_ang(self,Theta):
         if self.aver_Theta<Theta:#Theta가 0이상의 값 (오른쪽)
             if Theta>self.max_Theta:
                 steer_angle = 0.9
@@ -110,7 +84,6 @@ class LaneDetectorWebot:
         
         return steer_angle
 
-
     def detect_lane_lines(self,image): # 차선 검출 함수
     # 이미지 크기 추출
         height, width = image.shape[:2]
@@ -124,8 +97,6 @@ class LaneDetectorWebot:
         roi_vertices = [(0, height), (width // 2, height // 2), (width, height)]
         mask = np.zeros_like(edges)
         cv2.fillPoly(mask, np.array([roi_vertices], dtype=np.int32), 255)
-        # cv2.imshow("mask",mask)
-        # cv2.imshow("edges",edges)
         masked_image = cv2.bitwise_and(edges, mask)#mask는 관심구역 edges는 edge검출한 list
         # 허프 변환 (Hough Transform)을 사용하여 직선 검출
         lines = cv2.HoughLinesP(masked_image, rho=1, theta=np.pi/180, threshold=5, minLineLength=100, maxLineGap=50)# 
@@ -133,22 +104,11 @@ class LaneDetectorWebot:
 
         # 차선을 그릴 빈 이미지 생성
         line_image = np.zeros_like(image)
-
-        # 검출된 직선을 이미지에 그리기
-        # for line in lines:
-            # x1, y1, x2, y2 = line[0]
-            # cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=5)
-        # if lines is None:
-            # return []
         
         # 검출된 직선들을 하나의 다각형으로 그려 이진화된 이미지 생성
         if lines is not None:
             line_polygons = [lines.squeeze()]
             cv2.polylines(line_image, line_polygons, isClosed=False, color=(255, 255, 255), thickness=5)
-
-        # cv2.imshow("line",line_image)
-        # 원본 이미지와 차선 이미지를 합쳐서 반환
-        # result_image = cv2.addWeighted(image, 0.8, line_image, 1, 0)
 
         return line_image
     
@@ -165,10 +125,7 @@ class LaneDetectorWebot:
         print("right_pixel",right_pixel_sum)
         print("sum_pixel",left_pixel_sum+right_pixel_sum)
         
-        # if left_pixel_sum>9000
         Theta_right = (right_top_x - right_bottom_x) / (self.win_top_y - self.win_bottom_y)
-        # Theta_left = (left_top_x - left_bottom_x) / (self.win_top_y - self.win_bottom_y)
-        # print("ang",Theta_left)
 
         if right_pixel_sum<90000:
             Theta_left = (left_top_x - left_bottom_x) / (self.win_top_y - self.win_bottom_y)
@@ -176,21 +133,6 @@ class LaneDetectorWebot:
             return Theta_left
         print("right")
         return Theta_right
-    
-    def find_ref_distance(self, left_x_base, right_x_base):
-        ref_D_left = (320 - left_x_base) * -1
-        ref_D_right = right_x_base - 320
-        print("ref_D_left",ref_D_left)
-        print("ref_D_right",ref_D_right)
-        return ref_D_left + ref_D_right
-
-    def find_ref_angle(self, left_bottom_x, left_top_x, right_bottom_x, right_top_x):
-        win_bottom_y = 0  # fixed value
-        win_top_y = 432  # fixed value
-        Theta_left = (left_top_x - left_bottom_x) / (win_top_y - win_bottom_y)
-        Theta_right = (right_top_x - right_bottom_x) / (win_top_y - win_bottom_y)
-        print("left_ang",Theta_left)
-        return Theta_left - Theta_right
 
     def sliding_windows(self, binary_img,left_x_base,right_x_base):
         # The sliding windows logic goes here
@@ -276,8 +218,6 @@ class LaneDetectorWebot:
             return out_img, 0, 0, 0, 0
         
         ploty = np.linspace(0, binary_img.shape[0] - 1, binary_img.shape[0])
-        # left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
-        # right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
         out_img[non_zero_y[left_lane_indices], non_zero_x[left_lane_indices]] = [255, 255, 255]
         out_img[non_zero_y[right_lane_indices], non_zero_x[right_lane_indices]] = [255, 255, 255]
 
@@ -307,49 +247,9 @@ class LaneDetectorWebot:
         warp_frame = cv2.warpPerspective(frame,M,(width,height))
         return warp_frame
 
-    # def process_frame(self): ##여기선 사용하지 않음 -> image callback함수에서 process_frame역할 대체
-        # while self.video.isOpened():
-            # ret, frame = self.video.read()
-# 
-            # if not ret:
-                # break
-# 
-            # height, width = frame.shape[:2]
-            # result_hough = self.detect_lane_lines(frame)
-            # cv2.imshow("result_hough",result_hough)
-            # bird_eye_view = self.bird_eye_view_transform(frame, height, width)
-            # bird_line_view = self.image_filtering(bird_eye_view)
-# 
-            # cv2.imshow("Video", frame)
-            # cv2.imshow("bird_eye_view", bird_eye_view)
-# 
-            # left_x_base, right_x_base = self.histogram(bird_line_view)
-            # out_image, left_bottom_x, left_top_x, right_bottom_x, right_top_x = self.sliding_windows(bird_line_view,left_x_base, right_x_base)
-            # ref_distance = self.find_ref_distance(left_x_base, right_x_base)
-            # ref_angle = self.find_ref_angle(left_bottom_x, left_top_x, right_bottom_x, right_top_x)
-            # steer_angle = (self.k1 * ref_distance) + (self.k2 * ref_angle)
-            # print("steer_angle = ", steer_angle)
-# 
-            # if 0.0 < steer_angle < 1.0:
-                # steer_angle_msg = Float64()
-                # steer_angle_msg.data = steer_angle
-                # self.steer_angle_pub.publish(steer_angle_msg)
-                # self.rate.sleep()
-# 
-            # cv2.imshow("Result", out_image)
-# 
-            # if cv2.waitKey(30) & 0xFF == ord('q'):
-                # break
-# 
-        # self.video.release()
-        # cv2.destroyAllWindows()
 def main():
     sub_image = LaneDetectorWebot()
     rospy.spin()
 
-    # sub_image.image_CB()
-    # sub_image.process_frame()
-
 if __name__ == "__main__":
     main()
-    # lane_detector = LaneDetectorWebot()
